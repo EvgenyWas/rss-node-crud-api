cff8f6a87be9955ad073ec2d14bcea4a09194e3b
EvgenyWas <evgeniywas@gmail.com>
2024-10-18 17:32:30 +0100

    feat: add dictionary messages

---
+++ src/messages.ts
@@ -0,0 +1,10 @@
+export const messages = {
+  badRequest: 'Bad Request',
+  internalServerError: 'Internal Server Error',
+  unknownEndpoint: 'The endpoint does not exist',
+  invalidPayload: 'Payload is invalid',
+  invalidPayloadFields: 'Payload does not contain required fields',
+  invalidUserID: 'User ID is invalid',
+  userNotFound: 'User is not found',
+  forbidden: 'Forbidden',
+};
